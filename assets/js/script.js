5a4236dd99540ae6c8c2e6a7b2bc03724c13c04b
Mysmelf <matthewr9937@gmail.com>
2023-09-14 13:11:11 -0700

    Created branch, trying to get api to load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
 // for weather api call format:
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 // documentation at: https://openweathermap.org/forecast5#name5
-// my api key is: c57cbf4f07cb44866a70fe1c66c5d6fb
+
+let weatherUrl = 'api.openweathermap.org/data/2.5/weather?q=Seattle&appid=c57cbf4f07cb44866a70fe1c66c5d6fb';
+// Change Austin to a different city for a different result
+
+// Api Call Logic, log data to console
+function getWeather(url) {
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+// Call Api function
+getWeather(weatherUrl);
