cdb41f9f14fdc6b27196df9b4a70b5628a9b65b1
Mysmelf <matthewr9937@gmail.com>
2024-01-02 18:13:19 -0800

    Added additional commentation planning out the remainder of the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // for weather api call format:
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={long}&appid={API key}
 // documentation at: https://openweathermap.org/forecast5#name5
+// api for finding lat and lon at: https://openweathermap.org/api/geocoding-api
 
 let weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=c57cbf4f07cb44866a70fe1c66c5d6fb';
 // Change Austin to a different city for a different result
@@ -20,3 +21,20 @@ function getWeather(url) {
 
 // Call Api function
 getWeather(weatherUrl);
+
+
+
+
+// TODOS:
+
+// Create input that allows users to search for a city, which is then passed through the geocoding api
+// THEN extract geocoding api response lon & lat to be used in the 5-day-forcast API req
+// THEN display the response on the webpage for the next 5 days of weather for that city.
+
+// THINGS TO KEEP IN MIND:
+// using bootstrap
+// using jquery
+// persistant data is kept with local storage
+// include a history of perviously searched cities
+// when clicking on cities in your history, it performs a new api call ->
+// <- meaning we need to save city name and lon/lat to local storage for reuse (name for display, lon/lat for new api req)
